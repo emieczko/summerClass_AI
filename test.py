a0465c78d09e1e48fe6c9bf48df8e5d598a45ad3
ethan <ethanmieczkowski@gmail.com>
2022-07-20 16:40:10 -0400

    classes practice problem

---
+++ test.py
@@ -0,0 +1,32 @@
+class register:
+    def __init__(self, name, courses):
+        self.Sname = name
+        self.courses = courses
+    
+    def func(self):
+        iter = 0
+        print(self.Sname, "takes ", end='')
+        for c in self.courses:
+            iter += 1
+            if iter == 1:
+                print(c + ", ", end='')
+            elif iter < len(self.courses):
+                print(c + ", ", end='')
+            else:
+                print("and", c)
+        print("")
+
+class student:
+    def __init__(self, name, age, grade, adress):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.address = adress
+    
+    def studentFunc(self):
+        print(self.name, "is", self.age, "years old, in grade", self.grade + ", and lives on",self.address)
+
+student1 = student("Bob",10,"4","10 Example Road")
+student1.studentFunc()
+s1Register = register(student1.name, ["math", "english", "science", "history", "art", "music"])
+s1Register.func()

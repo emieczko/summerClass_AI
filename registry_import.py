bf0171524389ab0714816ecbce293684aea1aa7f
ethan <ethanmieczkowski@gmail.com>
2022-07-21 16:45:42 -0400

    some new stuff

---
+++ registry_import.py
@@ -0,0 +1,7 @@
+import registry
+
+student1 = registry.student("bob",9,"3","177 test street")
+student1.studentFunc()
+
+s1register = registry.register(student1.name, ["art","music","history","math 1","math 2","writing"])
+s1register.func()

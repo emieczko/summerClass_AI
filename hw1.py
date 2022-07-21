0eaab5779ed3c04d6aa526251fbde40d039bad1b
ethan <ethanmieczkowski@gmail.com>
2022-07-21 16:20:59 -0400

    added some comments

--- hw1.py
+++ hw1.py
@@ -15,13 +15,17 @@ def letterCounter(word):
     for l in word:
         nums += 1
     return nums
-
+#to choose which program to run
 program = input("select program 1, 2, or 3: ")
+#for hours to minutes conversion
 if program == "1":
     print(hrsToMins(int(input("enter hours: "))))
+#for hours to minutes or minutes to hours
 elif program == "2":
     print(timeConverter(input("enter unit: "), int(input("enter time: "))))
+#for counting letters in word
 elif program == "3":
     print(letterCounter(input("enter word: ")))
+#crude failsafe
 else:
     print("invalid program selection")
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#registry class
 class register:
     def __init__(self, name, courses):
         self.Sname = name
@@ -6,6 +7,7 @@ class register:
     def func(self):
         iter = 0
         print(self.Sname, "takes ", end='')
+        #really ugly way to make the output look pretty
         for c in self.courses:
             iter += 1
             if iter == 1:
@@ -15,7 +17,7 @@ class register:
             else:
                 print("and", c)
         print("")
-
+#student class
 class student:
     def __init__(self, name, age, grade, address):
         self.name = name
@@ -26,7 +28,7 @@ class student:
     def studentFunc(self):
         print(self.name, "is", self.age, "years old, in grade", self.grade + ", and lives on",self.address)
 
-student1 = student("Bob",10,"4","10 Example Road")
+student1 = student("Bob",10,"4","10 Example Road") #attributes for student class
 student1.studentFunc()
-s1Register = register(student1.name, ["math", "english", "science", "history", "art", "music"])
+s1Register = register(student1.name, ["math", "english", "science", "history", "art", "music"]) #attributes for registry class
 s1Register.func()
